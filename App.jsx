215bd9a28f56ba9c70977a0f1755c0bcec1d43b3
h3770k1k1 <amagdarass@gmail.com>
2024-08-12 16:48:35 +0200

    add ExitMenu component

--- App.jsx
+++ App.jsx
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 import AreaSubpage from './components/AreaSubpage';
 import Navbar from './components/Navbar';
 import BottomMenu from './components/BottomMenu';
+import ExitView from './components/ExitView';
+
 
 function App(): React.JSX.Element {
   const [activeColor, setActiveColor] = useState('#FFF');
 
   return (
     <>
-      <Navbar onColorChange={setActiveColor} />
-      <AreaSubpage activeColor={activeColor} />
+<ExitView/>
       <BottomMenu />
     </>
   );
---
+++ components/MenuOption.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import Arrow from './Arrow';
+
+const colors = ['#FFD3FA', '#FFE2CC', '#FFF7CC', '#D3F2D7', '#CDF6FF'];
+
+const Option = ({ text, index }) => {
+    const words = text.split(' ');
+    const lastWord = words.pop();
+    const remainingText = words.join(' ')+' ';
+
+    const lastWordStyle = index < colors.length ?
+        { backgroundColor: colors[index] } :
+        {};
+
+    return (
+        <TouchableOpacity style={styles.option}>
+            <Text style={styles.optionText}>
+                {remainingText}
+                {index === colors.length ? (
+                    <LinearGradient colors={colors} style={styles.gradientText} start={{ x: 0, y: 0 }}  end={{ x: 1, y: 0 }} >
+                        <Text style={styles.gradientWord}>{lastWord}</Text>
+                    </LinearGradient>
+                ) : (
+                    <Text style={[styles.lastWord, lastWordStyle]}>{lastWord}</Text>
+                )}
+            </Text>
+            <Arrow />
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    option: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        maxWidth: 400,
+        margin: '0 auto',
+        padding: 20,
+        borderBottomWidth: 2,
+        borderBottomColor: '#F0F0F0',
+    },
+    optionText: {
+        fontSize: 18,
+        width: 140,
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    lastWord: {
+        fontSize: 18,
+        fontWeight: 'medium',
+    },
+    gradientText: {
+        borderRadius: 4,
+        padding: 2,
+    },
+    gradientWord: {
+        fontSize: 18,
+        fontWeight: 'medium',
+        color: '#000',
+    },
+});
+
+export default Option;
