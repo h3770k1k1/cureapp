32692bec42cf9517f664a5c23ab7914c32a5020f
h3770k1k1 <amagdarass@gmail.com>
2024-08-15 15:06:04 +0200

    delete variable

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import BottomMenu from "./components/BottomMenu";
 
 function App(): React.JSX.Element {
-  const [activeColor, setActiveColor] = useState("#FFF");
-
   return (
     <>
       <BottomMenu />
