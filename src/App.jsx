6608ce6f8458ce66962d3c6b0b4b68759cb84a7a
Magdalena Raś <87210052+h3770k1k1@users.noreply.github.com>
2024-08-15 17:48:50 +0200

    Bottom menu (#2)
    * bottom menu
    
    * fixed app.js
    
    * delete variable
    
    * move styles to top
    
    * move colors to constants
    
    * delete files
    
    * bottommenu refactor

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import BottomMenu from "./components/BottomMenu";
+
+function App(): React.JSX.Element {
+  return (
+    <>
+      <BottomMenu />
+    </>
+  );
+}
+
+export default App;
