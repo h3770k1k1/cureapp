f00d47e92088363c5f0ca3ac2059e8647eaf7dbd
h3770k1k1 <amagdarass@gmail.com>
2024-08-26 10:42:26 +0200

    exitview

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import ExitView from "./components/ExitView";
+
+function App(): React.JSX.Element {
+
+  return (
+    <>
+      <ExitView />
+    </>
+  );
+}
+
+export default App;
