900f51e52e5aa020da07ef0c1609ca04eacff335
h3770k1k1 <amagdarass@gmail.com>
2024-09-09 07:50:10 +0200

    after merge commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-
+import React, { useState } from "react";
 import AreaSubpage from "./components/AreaSubpage";
 import Navbar from "./components/Navbar";
 import BottomMenu from "./components/BottomMenu";
 
-
-function App(): React.JSX.Element {
+function App(): JSX.Element {
   const [activeColor, setActiveColor] = useState("#FFF");
 
   return (
     <>
       <Navbar onColorChange={setActiveColor} />
       <AreaSubpage activeColor={activeColor} />
-       <BottomMenu />
+      <BottomMenu />
     </>
   );
 }
