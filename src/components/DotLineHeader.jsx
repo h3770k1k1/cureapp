ab8296f0dcbaf6b437f7d5bf2d56964274a4e04a
h3770k1k1 <amagdarass@gmail.com>
2024-09-10 18:43:12 +0200

    update dotlineheader component

--- src/components/DotLineHeader.jsx
+++ src/components/DotLineHeader.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const DotLineHeader = ({ text }) => {
-  const { width: screenWidth } = Dimensions.get('window');
-  const textWidth = 16 * text.length;
-  const dotsWidth = screenWidth - textWidth - 100;
-  const dotSize = 10;
-  const dotCount = 4;
+  const totalLength = 40; // Całkowita liczba znaków
+  const dotsBefore = 4; // Zawsze 4 kropki przed tekstem
+  const textLength = text.length;
 
-  const dots = '.'.repeat(dotCount);
+  // Obliczamy, ile kropek potrzebujemy po tekście, aby łącznie było 40 znaków
+  const dotsAfterCount = totalLength - textLength - dotsBefore;
+  const dotsBeforeText = '.'.repeat(dotsBefore);
+  const dotsAfterText = '.'.repeat(dotsAfterCount > 0 ? dotsAfterCount : 0);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.dots}>{dots}</Text>
+      <Text style={styles.dots}>{dotsBeforeText}</Text>
       <Text style={styles.text}>{text}</Text>
-      <Text style={styles.dots}>{dots}</Text>
+      <Text style={styles.dots}>{dotsAfterText}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-  width:'100%',
+    width: '100%',
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
@@ -30,14 +31,14 @@ const styles = StyleSheet.create({
   dots: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom:10,
+    marginBottom: 10,
   },
   text: {
     fontSize: 16,
     fontStyle: 'italic',
     fontWeight: 'bold',
     marginHorizontal: 10,
-     textTransform: 'uppercase',
+    textTransform: 'uppercase',
   },
 });
 
