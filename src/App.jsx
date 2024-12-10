e701caf9577be6ee4c495dec7a7defe31993feb2
h3770k1k1 <amagdarass@gmail.com>
2024-12-10 23:20:54 +0100

    fixing naigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,24 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Mental from "./Views/Mental";
-import Home from "./Views/Home";
-import BodyArticle1 from "./Views/Articles/Cialo/BodyArticle1";
-import BodyArticle2 from "./Views/Articles/Cialo/BodyArticle2";
-import BodyArticle3 from "./Views/Articles/Cialo/BodyArticle3";
-import BodyArticle4 from "./Views/Articles/Cialo/BodyArticle4";
-import BodyArticle5 from "./Views/Articles/Cialo/BodyArticle5";
-import BodyArticle6 from "./Views/Articles/Cialo/BodyArticle6";
-import MindArticle1 from "./Views/Articles/Umysl/MindArticle1";
-import MindArticle2 from "./Views/Articles/Umysl/MindArticle2";
-import MindArticle3 from "./Views/Articles/Umysl/MindArticle3";
-import MindArticle4 from "./Views/Articles/Umysl/MindArticle4";
-import RelationshipsArticle1 from "./Views/Articles/Relacje/RelationshipsArticle1";
-import RelationshipsArticle2 from "./Views/Articles/Relacje/RelationshipsArticle2";
-import RelationshipsArticle3 from "./Views/Articles/Relacje/RelationshipsArticle3";
-import RelationshipsArticle4 from "./Views/Articles/Relacje/RelationshipsArticle4";
-import RelationshipsArticle5 from "./Views/Articles/Relacje/RelationshipsArticle5";
-import RelationshipsArticle6 from "./Views/Articles/Relacje/RelationshipsArticle6";
-import EmotionsArticle1 from "./Views/Articles/Emocje/EmotionsArticle1";
-import EmotionsArticle2 from "./Views/Articles/Emocje/EmotionsArticle2";
-import EmotionsArticle3 from "./Views/Articles/Emocje/EmotionsArticle3";
-import EmotionsArticle4 from "./Views/Articles/Emocje/EmotionsArticle4";
-import SoulArticle1 from "./Views/Articles/Dusza/SoulArticle1";
-import SoulArticle2 from "./Views/Articles/Dusza/SoulArticle2";
-import SoulArticle3 from "./Views/Articles/Dusza/SoulArticle3";
-import SoulArticle4 from "./Views/Articles/Dusza/SoulArticle4";
-import SoulArticle5 from "./Views/Articles/Dusza/SoulArticle5";
-import SoulArticle6 from "./Views/Articles/Dusza/SoulArticle6";
+import Home from './Views/Home';
+import Mental from './Views/Mental';
+import Articles from "./Views/Articles/Articles";
+import articles from './articles.json';
 
 const Stack = createStackNavigator();
+
+const generateArticleScreens = () => {
+  return Object.entries(articles).flatMap(([category, articleNames]) =>
+    articleNames.map((articleName) => (
+      <Stack.Screen
+        key={articleName}
+        name={articleName}
+        component={Articles[articleName]}
+      />
+    ))
+  );
+};
 
 const App = () => {
   return (
@@ -38,60 +26,7 @@ const App = () => {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Mental" component={Mental} />
-
-        {/* Ciało */}
-        <Stack.Screen name="BodyArticle1" component={BodyArticle1} />
-        <Stack.Screen name="BodyArticle2" component={BodyArticle2} />
-        <Stack.Screen name="BodyArticle3" component={BodyArticle3} />
-        <Stack.Screen name="BodyArticle4" component={BodyArticle4} />
-        <Stack.Screen name="BodyArticle5" component={BodyArticle5} />
-        <Stack.Screen name="BodyArticle6" component={BodyArticle6} />
-
-        {/* Umysł */}
-        <Stack.Screen name="MindArticle1" component={MindArticle1} />
-        <Stack.Screen name="MindArticle2" component={MindArticle2} />
-        <Stack.Screen name="MindArticle3" component={MindArticle3} />
-        <Stack.Screen name="MindArticle4" component={MindArticle4} />
-
-        {/* Relacje */}
-        <Stack.Screen
-          name="RelationshipsArticle1"
-          component={RelationshipsArticle1}
-        />
-        <Stack.Screen
-          name="RelationshipsArticle2"
-          component={RelationshipsArticle2}
-        />
-        <Stack.Screen
-          name="RelationshipsArticle3"
-          component={RelationshipsArticle3}
-        />
-        <Stack.Screen
-          name="RelationshipsArticle4"
-          component={RelationshipsArticle4}
-        />
-        <Stack.Screen
-          name="RelationshipsArticle5"
-          component={RelationshipsArticle5}
-        />
-        <Stack.Screen
-          name="RelationshipsArticle6"
-          component={RelationshipsArticle6}
-        />
-
-        {/* Emocje */}
-        <Stack.Screen name="EmotionsArticle1" component={EmotionsArticle1} />
-        <Stack.Screen name="EmotionsArticle2" component={EmotionsArticle2} />
-        <Stack.Screen name="EmotionsArticle3" component={EmotionsArticle3} />
-        <Stack.Screen name="EmotionsArticle4" component={EmotionsArticle4} />
-
-        {/* Dusza */}
-        <Stack.Screen name="SoulArticle1" component={SoulArticle1} />
-        <Stack.Screen name="SoulArticle2" component={SoulArticle2} />
-        <Stack.Screen name="SoulArticle3" component={SoulArticle3} />
-        <Stack.Screen name="SoulArticle4" component={SoulArticle4} />
-        <Stack.Screen name="SoulArticle5" component={SoulArticle5} />
-        <Stack.Screen name="SoulArticle6" component={SoulArticle6} />
+        {generateArticleScreens()}
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/Views/Articles/Articles.jsx
@@ -0,0 +1,69 @@
+
+// Ciało
+import BodyArticle1 from "./Cialo/BodyArticle1";
+import BodyArticle2 from "./Cialo/BodyArticle2";
+import BodyArticle3 from "./Cialo/BodyArticle3";
+import BodyArticle4 from "./Cialo/BodyArticle4";
+import BodyArticle5 from "./Cialo/BodyArticle5";
+import BodyArticle6 from "./Cialo/BodyArticle6";
+
+// Umysł
+import MindArticle1 from "./Umysl/MindArticle1";
+import MindArticle2 from "./Umysl/MindArticle2";
+import MindArticle3 from "./Umysl/MindArticle3";
+import MindArticle4 from "./Umysl/MindArticle4";
+
+// Relacje
+import RelationshipsArticle1 from "./Relacje/RelationshipsArticle1";
+import RelationshipsArticle2 from "./Relacje/RelationshipsArticle2";
+import RelationshipsArticle3 from "./Relacje/RelationshipsArticle3";
+import RelationshipsArticle4 from "./Relacje/RelationshipsArticle4";
+import RelationshipsArticle5 from "./Relacje/RelationshipsArticle5";
+import RelationshipsArticle6 from "./Relacje/RelationshipsArticle6";
+
+// Emocje
+import EmotionsArticle1 from "./Emocje/EmotionsArticle1";
+import EmotionsArticle2 from "./Emocje/EmotionsArticle2";
+import EmotionsArticle3 from "./Emocje/EmotionsArticle3";
+import EmotionsArticle4 from "./Emocje/EmotionsArticle4";
+
+// Dusza
+import SoulArticle1 from "./Dusza/SoulArticle1";
+import SoulArticle2 from "./Dusza/SoulArticle2";
+import SoulArticle3 from "./Dusza/SoulArticle3";
+import SoulArticle4 from "./Dusza/SoulArticle4";
+import SoulArticle5 from "./Dusza/SoulArticle5";
+import SoulArticle6 from "./Dusza/SoulArticle6";
+
+// Eksportujemy obiekt komponentów
+const Articles = {
+
+  BodyArticle1,
+  BodyArticle2,
+  BodyArticle3,
+  BodyArticle4,
+  BodyArticle5,
+  BodyArticle6,
+  MindArticle1,
+  MindArticle2,
+  MindArticle3,
+  MindArticle4,
+  RelationshipsArticle1,
+  RelationshipsArticle2,
+  RelationshipsArticle3,
+  RelationshipsArticle4,
+  RelationshipsArticle5,
+  RelationshipsArticle6,
+  EmotionsArticle1,
+  EmotionsArticle2,
+  EmotionsArticle3,
+  EmotionsArticle4,
+  SoulArticle1,
+  SoulArticle2,
+  SoulArticle3,
+  SoulArticle4,
+  SoulArticle5,
+  SoulArticle6,
+};
+
+export default Articles;
